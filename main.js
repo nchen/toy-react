e1f052e9b70f3f96927fbcb24199ec7e03a2920f
woodfox <woodfox@wfmbp.local>
2020-09-29 01:09:17 +0800

    Use range API to render to DOM

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import { createElement, Component, render } from "./toy-react";
 
 class MyComponent extends Component {
+  constructor(){
+    super();
+    this.state = {
+      a: 1,
+      b: 2
+    }
+  }
   render() {
     return (
       <div>
         <h1>This is my component</h1>
+        <span>{this.state.a.toString()}</span>
         {this.children}
       </div>
     );
--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,5 @@
+const RENDER_TO_DOM = Symbol("render to dom");
+
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type);
@@ -8,13 +10,24 @@ class ElementWrapper {
   }
 
   appendChild(component) {
-    this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    component[RENDER_TO_DOM](range);
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -33,11 +46,8 @@ export class Component {
     this.children.push(component);
   }
 
-  get root() {
-    if (!this._root) {
-      this._root = this.render().root;
-    }
-    return this._root;
+  [RENDER_TO_DOM](range) {
+    this.render()[RENDER_TO_DOM](range);
   }
 }
 
@@ -72,5 +82,9 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root);
+  let range = document.createRange();
+  range.setStart(parentElement, 0);
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  range.deleteContents();
+  component[RENDER_TO_DOM](range);
 }
